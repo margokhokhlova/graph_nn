87a3fc826b7bf8942156200cde4ce6bcaf7d850c
margokhokhlova <tysyisya@gmail.com>
2020-05-12 13:01:26 +0200

    tensor board logging added
    the model trains on several datasets available (4 in total) and logs the data

--- siamese_main.py
+++ siamese_main.py
@@ -1,22 +1,11 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import copy
-import time
-import math
-import torch
 import torch.utils
 import torch.utils.data
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch.optim.lr_scheduler as lr_scheduler
-from torch.nn.parameter import Parameter
-from os.path import join as pjoin
-
+import argparse
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
 from data_loader_siamese import *
 from loss import contrastive_loss
-
+from knn_check import knn_distance_calculation
 
 # NN layers and models
 class GraphConv(nn.Module):
@@ -41,7 +30,7 @@ class GraphConv(nn.Module):
         batch, N = A.shape[:2]
         if self.adj_sq:
             A = torch.bmm(A, A)  # use A^2 to increase graph connectivity
-        I = torch.eye(N).unsqueeze(0).to(device)
+        I = torch.eye(N).unsqueeze(0).to(args.device)
         if self.scale_identity:
             I = 2 * I  # increase weight of self connections
         A_hat = A + I
@@ -103,61 +92,90 @@ class GCN(nn.Module):
 
 if __name__ == '__main__':
     # Experiment parameters
-    batch_size = 5
-    threads = 0
-    lr = 0.005
-    epochs = 40
-    log_interval = 10
-    wdecay = 1e-4
-    emb_dim = 128  # I am trying to get an embedding in the end
-    dataset = 'ign_2004'
-    dataset2 = 'ign_2019'
-    model_name = 'gcn'  # 'gcn', 'unet'
-    device = 'cpu'  # 'cuda', 'cpu'
-    visualize = True
-    shuffle_nodes = False
-    n_folds = 2  # 10-fold cross validation
-    seed = 111
-    tau = 0.5
+    parser = argparse.ArgumentParser(description='contrastive_GCN')
+
+    parser.add_argument('--first_dataset', type=str, default='ign_2019',
+                        help='Name of dataset number 1, should correspond to the folder with data')
+    parser.add_argument('--second_dataset', type=str, default='ign_2014',
+                        help='Name of the matching dataset, should correspond to the folder with data')
+    parser.add_argument('--third_dataset', type=str, default='ign_2010',
+                        help='Name of the matching dataset, should correspond to the folder with data')
+    parser.add_argument('--testing_dataset', type=str, default='ign_2004',
+                        help='Name of the matching dataset, should correspond to the folder with data')
+    parser.add_argument('--emb_dim', type=int, default=128,
+                        help='Feature output size (default: 128')
+    parser.add_argument('--hidden_filters', type=list, default=[128, 256],
+                        help='num of gcn layers')
+    parser.add_argument('--batch-size', type=int, default=35, metavar='N',
+                        help='input training batch-size')
+    parser.add_argument('--epochs', type=int, default=45, metavar='N',
+                        help='number of training epochs (default: 150)')
+    parser.add_argument('--lr', type=float, default=1e-3,
+                        help='learning rate (default: 1e-3')
+    parser.add_argument("--decay-lr", default=1e-6, action="store", type=float,
+                        help='Learning rate decay (default: 1e-6')
+    parser.add_argument('--tau', default=0.5, type=float,
+                        help='Tau temperature smoothing (default 0.5)')
+    parser.add_argument('--log-dir', type=str, default='runs',
+                        help='logging directory (default: runs)')
+    parser.add_argument('--device', default='cpu',
+                        help='cpu or cuda')
+    parser.add_argument('--load-model', type=str, default=None,
+                        help='Load model to resume training for (default None)')
+    parser.add_argument('--device-id', type=int, default=0,
+                        help='GPU device id (default: 0')
+    parser.add_argument('--threads', type=int, default=0,
+                        help='num of threads')
+    parser.add_argument('--log_interval', type=int, default=10,
+                        help='num of threads')
+    parser.add_argument('--n_folds', type=int, default=2,
+                        help='n-fold cross validation, default is 2 folds - single check of best val accuracy')
+    parser.add_argument('--seed', type=int, default=111,
+                        help='seed for reproduction')
+    parser.add_argument('--visualize', type=bool, default=True,
+                        help='visualize the data')
+
+    args = parser.parse_args()
+    filename = args.log_dir + '/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.txt'
+
+    w = open(filename, "w")
+    for key, val in args.__dict__.items():
+        w.write(str(key) +' ' +str(val) +'\n')
     print('torch', torch.__version__)
 
-
     print('Loading data')
-    datareader = DataReader(data_dir='./data/%s/' % dataset.upper(),
-                            rnd_state=np.random.RandomState(seed),
-                            folds=n_folds,
+    datareader19 = DataReader(data_dir='./data/%s/' % args.first_dataset.upper(),
+                            rnd_state=np.random.RandomState(args.seed),
+                            folds=args.n_folds,
                             use_cont_node_attr=True)
-    datareader19 = DataReader(data_dir='./data/%s/' % dataset2.upper(),
-                              rnd_state=np.random.RandomState(seed),
-                              folds=n_folds,
+    datareader14 = DataReader(data_dir='./data/%s/' % args.second_dataset.upper(),
+                              rnd_state=np.random.RandomState(args.seed),
+                              folds=args.n_folds,
                               use_cont_node_attr=True)
+    datareader10 = DataReader(data_dir='./data/%s/' % args.third_dataset.upper(),
+                              rnd_state=np.random.RandomState(args.seed),
+                              folds=args.n_folds,
+                              use_cont_node_attr=True)
+    datareader04 = DataReader(data_dir='./data/%s/' % args.testing_dataset.upper(),
+                              rnd_state=np.random.RandomState(args.seed),
+                              folds=args.n_folds,
+                              use_cont_node_attr=True)
+    # tensorboard
+    writer = SummaryWriter()
 
-    acc_folds = []
-    for fold_id in range(n_folds):
-        print('\nFOLD', fold_id)
-        loaders = []
-        for split in ['train', 'test']:
-            gdata = GraphDataSiamese(fold_id=fold_id,
-                                     datareader04=datareader, datareader19=datareader19,
-                                     split=split)
+    # model definition
 
-            loader = torch.utils.data.DataLoader(gdata,
-                                                 batch_size=batch_size,
-                                                 # shuffle=split.find('train') >= 0,
-                                                 num_workers=threads)
-            loaders.append(loader)
-
-    if model_name == 'gcn':
-        model = GCN(in_features=loaders[0].dataset.features_dim,
-                    out_features=emb_dim,  # loaders[0].dataset.n_classes
-                    n_hidden=0,
-                    filters=[64, 64, 64],
-                    dropout=0.2,
-                    adj_sq=False,
-                    scale_identity=False).to(device)
+    model = GCN(in_features=datareader19.data['features_dim'],
+                out_features=args.emb_dim,  # loaders[0].dataset.n_classes
+                n_hidden=0,
+                filters=args.hidden_filters,
+                dropout=0.2,
+                adj_sq=False,
+                scale_identity=True).to(args.device)
 
     print('\nInitialize model')
     print(model)
+
     c = 0
     for p in filter(lambda p: p.requires_grad, model.parameters()):
         c += p.numel()
@@ -165,23 +183,23 @@ if __name__ == '__main__':
 
     optimizer = optim.Adam(
         filter(lambda p: p.requires_grad, model.parameters()),
-        lr=lr,
-        weight_decay=wdecay,
+        lr=args.lr,
+        weight_decay=args.decay_lr,
         betas=(0.5, 0.999))
 
     scheduler = lr_scheduler.MultiStepLR(optimizer, [20, 30], gamma=0.1)
-
+    best_test_loss = 10000 # to keep track of the validation_loss parameter
 
     def train(train_loader, epoch):
         scheduler.step()
         model.train()
-        loss_func = contrastive_loss(tau=tau)
+        loss_func = contrastive_loss(tau=args.tau)
         start = time.time()
         train_loss, n_samples = 0, 0
         for batch_idx, data in enumerate(train_loader):
             for i in range(len(data[0])):
-                data[0][i] = data[0][i].to(device)
-                data[1][i] = data[1][i].to(device)
+                data[0][i] = data[0][i].to(args.device)
+                data[1][i] = data[1][i].to(args.device)
             optimizer.zero_grad()
             output_2004 = model(data[0])
             output_2019 = model(data[1])
@@ -191,28 +209,32 @@ if __name__ == '__main__':
             time_iter = time.time() - start
             train_loss += loss.item() * len(output_2004)
             n_samples += len(output_2004)
-            if batch_idx % log_interval == 0 or batch_idx == len(train_loader) - 1:
+            if batch_idx % args.log_interval == 0 or batch_idx == len(train_loader) - 1:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} (avg: {:.6f}) \tsec/iter: {:.4f}'.format(
                     epoch, n_samples, len(train_loader.dataset),
                     100. * (batch_idx + 1) / len(train_loader), loss.item(), train_loss / n_samples,
                     time_iter / (batch_idx + 1)))
-
+                w.write('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f} (avg: {:.6f}) \tsec/iter: {:.4f} \n'.format(
+                    epoch, n_samples, len(train_loader.dataset),
+                    100. * (batch_idx + 1) / len(train_loader), loss.item(), train_loss / n_samples,
+                    time_iter / (batch_idx + 1)))
+        return train_loss/(batch_idx * args.batch_size)
 
     #             break
 
     def test(test_loader, epoch):
         model.eval()
         start = time.time()
-        loss_fn = contrastive_loss(tau=tau)
+        loss_fn = contrastive_loss(tau=args.tau)
         test_loss, correct, n_samples = 0, 0, 0
         for batch_idx, data in enumerate(test_loader):
             for i in range(len(data[0])):
-                data[0][i] = data[0][i].to(device)
-                data[1][i] = data[1][i].to(device)
+                data[0][i] = data[0][i].to(args.device)
+                data[1][i] = data[1][i].to(args.device)
             optimizer.zero_grad()
             output_2004 = model(data[0])
             output_2019 = model(data[1])
-            loss = loss_fn(output_2004, output_2019, tau=tau)
+            loss = loss_fn(output_2004, output_2019)
             test_loss += loss.item()
             n_samples += len(output_2004)
         #         pred = output.detach().cpu().max(1, keepdim=True)[1]
@@ -225,17 +247,21 @@ if __name__ == '__main__':
 
         # acc = 100. * correct / n_samples
         print('Test set (epoch {}): Average loss: {:.4f}\n'.format(epoch,
-                                                                   test_loss))
+                                                                 test_loss))
+        global best_test_loss
+        if test_loss<best_test_loss:
+            best_test_loss = test_loss
+            torch.save(model.state_dict(), 'saved_model/siamese_gcn.pth') # save the best parameters
         return test_loss
 
 
-    def calculate_features(train_loader):
-        features2004 = np.empty((0, 128))
-        features2019 = np.empty((0, 128))
+    def calculate_features(train_loader, dims):
+        features2004 = np.empty((0, dims))
+        features2019 = np.empty((0, dims))
         for batch_idx, data in enumerate(train_loader):
             for i in range(len(data[0])):
-                data[0][i] = data[0][i].to(device)
-                data[1][i] = data[1][i].to(device)
+                data[0][i] = data[0][i].to(args.device)
+                data[1][i] = data[1][i].to(args.device)
             output_2004 = model(data[0])
             output_2019 = model(data[1])
             features_2004 = output_2004.detach().cpu()  # .max(1, keepdim=True)[1]
@@ -244,8 +270,83 @@ if __name__ == '__main__':
             features2019 = np.vstack(
                 (features2019, features_2019.data.numpy()))  # (features_2019.reshape(features_2019.shape[0], emb_dim))
         return np.array(features2004), np.array(features2019)
+    def cross_val_map(loaders):
+        emb_2004 = []
+        emb_2019 = []
+        emb04, emb19 = calculate_features(loaders[0], args.emb_dim)
+        emb_2004.append(emb04)
+        emb_2019.append(emb19)
+        emb04, emb19 = calculate_features(loaders[1], args.emb_dim)
+        emb_2004.append(emb04)
+        emb_2019.append(emb19)
+        emb_2004 = np.asarray(emb_2004).reshape(-1, args.emb_dim)
+        emb_2019 = np.asarray(emb_2019).reshape(-1, args.emb_dim)
+        map = knn_distance_calculation(query=emb_2004, database=emb_2019, distance='cosine', N=5)
+        return map
 
 
-    for epoch in range(epochs):
-        train(loaders[0], epoch)
-        test(loaders[0], epoch)
+
+
+
+    # first round of training
+    for fold_id in range(args.n_folds):
+        print('\nFOLD', fold_id)
+        loaders = []
+        loaders_val = []
+        for split in ['train', 'test']:
+            gdata = GraphDataSiamese(fold_id=fold_id,
+                                     datareader04=datareader19, datareader19=datareader14,
+                                     split=split)
+            gdata_val = GraphDataSiamese(fold_id=fold_id,
+                                     datareader04=datareader19, datareader19=datareader10,
+                                     split=split) #second validation set to calculate map
+
+            loader = torch.utils.data.DataLoader(gdata,
+                                                 batch_size=args.batch_size,
+                                                 # shuffle=split.find('train') >= 0,
+                                                 num_workers=args.threads)
+            loader_val = torch.utils.data.DataLoader(gdata_val,
+                                                 batch_size=args.batch_size,
+                                                 # shuffle=split.find('train') >= 0,
+                                                 num_workers=args.threads)
+            loaders.append(loader)
+            loaders_val.append(loader_val)
+        for epoch in range(args.epochs):
+            tr_l =train(loaders[0], epoch)
+            ts_l = test(loaders[1], epoch)
+            writer.add_scalar('Loss/train', tr_l, epoch)
+            writer.add_scalar('Loss/val', ts_l, epoch)
+            map = cross_val_map(loaders)
+            w.write('epoch' + str(epoch) + 'map on val set is ' + str(map) + '\n')
+            writer.add_scalar('map@train', map, epoch)
+            map = cross_val_map(loaders_val)
+            w.write('epoch'+str(epoch)+'map on val set is ' + str(map) + '\n')
+            writer.add_scalar('map@val', map, epoch)
+            for param_group in optimizer.param_groups:
+                lr  = param_group['lr']
+            writer.add_scalar('lr', lr, epoch)
+    # second round of training using the second dataset:
+    lr_scheduler.base_lrs = [args.lr]
+    for epoch in range(args.epochs, args.epochs*2):
+        train(loaders_val[0], epoch)
+        test(loaders_val[1], epoch)
+        map = cross_val_map(loaders)
+        w.write('epoch'+str(epoch)+'map on val set is ' + str(map) + '\n')
+
+    model.load_state_dict(torch.load('saved_model/siamese_gcn.pth'))
+
+    #final test accuracy calculation
+    test_loaders = []
+    for split in ['train', 'test']:
+        gdata = GraphDataSiamese(fold_id=0,
+                                 datareader04=datareader04, datareader19=datareader10, split = split)
+
+        test_loader = torch.utils.data.DataLoader(gdata,
+                                             batch_size=args.batch_size,
+                                             # shuffle=split.find('train') >= 0,
+                                             num_workers=args.threads)
+        test_loaders.append(test_loader)
+
+    map = cross_val_map(test_loaders)
+    w.write('final map precision is ' + str(map)+'\n')
+    w.close()
